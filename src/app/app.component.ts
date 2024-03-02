7146e22000f3d5037ab6c8e11b73a209657db436
Nathan Arvan <nathannarvan@gamil.com>
2024-03-02 14:44:10 -0600

    load and display list of maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { Map } from './models/map';
+import { MapServiceService } from './services/map-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ttrpgClient';
+  maps!: Map[]; 
+
+  constructor(
+    private mapService: MapServiceService,
+    private router: Router
+    ) {}
+
+  ngOnInit(): void {
+    this.mapService.getMaps().subscribe(maps => {
+      this.maps = maps;
+    })
+  }
+
+  mapClicked(mapId: number) {
+    this.router.navigate(['maps', mapId])
+  }
 }
