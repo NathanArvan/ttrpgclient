41435fc979d7c17fd2e01fe0b85ba50f4b71d40e
Nathan Arvan <nathannarvan@gamil.com>
2024-03-02 14:43:36 -0600

    update config and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MapViewComponent } from './map-view/map-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    component: MapViewComponent,
+    path: 'maps/:id'
+}];
