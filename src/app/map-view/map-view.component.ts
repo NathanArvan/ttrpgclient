c55dc4e2df51846b5c592e6d257aa0fa8412801f
Nathan Arvan <nathannarvan@gamil.com>
2024-03-03 13:44:36 -0600

    render map matrix with token logic

--- src/app/map-view/map-view.component.ts
+++ src/app/map-view/map-view.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MapServiceService } from '../services/map-service.service';
+import { ActivatedRoute } from '@angular/router';
+import { Map } from '../models/map';
+import { Token } from '../models/token';
+import { CommonModule } from '@angular/common';
+
+export interface MapCell {
+  token: Token | null
+}
 
 @Component({
   selector: 'app-map-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './map-view.component.html',
   styleUrl: './map-view.component.css'
 })
-export class MapViewComponent {
+export class MapViewComponent implements OnInit{
+  public map!: Map;
+  public mapMatrix: MapCell[][] = [];
 
+  constructor(
+    private mapService: MapServiceService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.url.subscribe(url => {
+      const mapId = url[url.length -1];
+      this.mapService.getMap((parseInt(mapId.toString()))).subscribe(map => {
+        this.map = map;
+        this.generateMapMatrix()
+      })
+    })
+  }
+
+  generateMapMatrix() {
+    this.mapMatrix = new Array(this.map.length);
+    for(let i =0; i < this.map.length; i++ ) {
+      this.mapMatrix[i] = new Array(this.map.width);
+      for(let j = 0; j < this.map.width; j++) {
+        this.mapMatrix[i][j] = {token: null}
+      }
+    }
+    this.map.tokens.forEach(token => {
+      console.log(token, this.mapMatrix);
+      console.log(this.mapMatrix[token.xPosition]);
+      this.mapMatrix[token.xPosition][token.yPosition].token = token; 
+    })
+  }
 }
--- src/app/models/token.ts
+++ src/app/models/token.ts
@@ -2,6 +2,6 @@ export interface Token {
     tokenId: number;
     mapId: number;
     CharacterId: number;
-    XPosition: number;
-    YPosition: number;
+    xPosition: number;
+    yPosition: number;
 }
