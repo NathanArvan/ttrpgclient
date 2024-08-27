bd9f4f8659fba28e22e804a53dbedc68602da694
Nathan Arvan <nathannarvan@gmail.com>
2024-08-27 15:24:04 -0500

    add navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Router, RouterOutlet } from '@angular/router';
 import { Map } from './models/map';
 import { MapService } from './services/map.service';
 import { CommonModule } from '@angular/common';
+import { NavigationBarComponent } from "./modules/shared/navigation-bar/navigation-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
