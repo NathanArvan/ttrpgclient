3ad6d390dc0538002ebd55fe654713f9a2c25a9f
Nathan Arvan <nathannarvan@gmail.com>
2024-08-30 14:49:01 -0500

    hello world phaser animation works

--- src/app/modules/test-pages/phaser-test-page/phaser-test-page.component.ts
+++ src/app/modules/test-pages/phaser-test-page/phaser-test-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Phaser from 'phaser';
 
 @Component({
   selector: 'app-phaser-test-page',
@@ -7,6 +8,56 @@ import { Component } from '@angular/core';
   templateUrl: './phaser-test-page.component.html',
   styleUrl: './phaser-test-page.component.css'
 })
-export class PhaserTestPageComponent {
+export class PhaserTestPageComponent implements OnInit {
+  public phaserGame!: Phaser.Game;
+  private config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: Example,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x:0, y: 200 }
+        }
+    }
+  };
 
+  constructor() {  }
+
+  ngOnInit(): void {
+    this.phaserGame = new Phaser.Game(this.config);
+  }
+}
+
+class Example extends Phaser.Scene {
+  preload ()
+  {
+      this.load.setBaseURL('https://labs.phaser.io');
+
+      this.load.image('sky', 'assets/skies/space3.png');
+      this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+      this.load.image('red', 'assets/particles/red.png');
+  }
+
+  create ()
+  {
+      this.add.image(400, 300, 'sky');
+
+      const particles = this.add.particles(0, 0, 'red', {
+          speed: 100,
+          scale: { start: 1, end: 0 },
+          blendMode: 'ADD'
+      });
+
+      const logo = this.physics.add.image(400, 100, 'logo');
+
+      logo.setVelocity(100, 200);
+      logo.setBounce(1, 1);
+      logo.setCollideWorldBounds(true);
+
+      particles.startFollow(logo);
+  }
+
+  
 }
