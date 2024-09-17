2d850b3342397020c580d20485ec245f53ad8279
Nathan Arvan <nathannarvan@gmail.com>
2024-09-17 16:29:23 -0500

    create multiplayer test page

---
+++ src/app/modules/test-pages/multiplayer-test-page/multiplayer-test-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, computed, signal } from '@angular/core';
+import { User } from '../../../models/user';
+import { Battle } from '../../../models/battle';
+
+@Component({
+  selector: 'app-multiplayer-test-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './multiplayer-test-page.component.html',
+  styleUrl: './multiplayer-test-page.component.css'
+})
+export class MultiplayerTestPageComponent {
+  public currentUser = signal<User | null>(null);
+  public userIsLoaded = computed(() => {
+    return this.currentUser() !== null;
+  })
+
+  public currentBattle = signal<Battle | null>(null);
+  public battleIsLoaded = computed(() => {
+    return this.currentBattle() !== null;
+  })
+}
