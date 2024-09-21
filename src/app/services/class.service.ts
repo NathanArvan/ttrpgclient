5fd1f39559d886441962c2e5510da72ec3e04a5b
Nathan Arvan <nathannarvan@gmail.com>
2024-09-21 16:15:03 -0500

    add class service

---
+++ src/app/services/class.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClassService {
+  private baseURL = environment.apiURL;
+
+  constructor(private client: HttpClient) { }
+
+  public getClasses() {
+    return this.client.get(`${this.baseURL}/classes/`)
+  }
+}
