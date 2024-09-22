bd2d663ef1b2813cb003e6db4f015b0aec72481e
Nathan Arvan <nathannarvan@gmail.com>
2024-09-22 10:29:45 -0500

    make user model fields nullable

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,8 +1,8 @@
 import { Character } from "./character";
 
 export interface User {
-    userId: number;
+    userId: number | null;
     name: string;
     email: string;
-    characters: Character[];
+    characters: Character[] | null;
 }
