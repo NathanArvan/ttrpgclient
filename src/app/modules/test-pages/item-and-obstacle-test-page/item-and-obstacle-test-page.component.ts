a0b963cac9e76e88ffa1c3faacfd53532fadd1ba
Nathan Arvan <nathannarvan@gmail.com>
2024-11-11 13:41:40 -0600

    add methods and data for create battle method

--- src/app/modules/test-pages/item-and-obstacle-test-page/item-and-obstacle-test-page.component.ts
+++ src/app/modules/test-pages/item-and-obstacle-test-page/item-and-obstacle-test-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BattleGridComponent } from '../../shared/battle-grid/battle-grid.component';
+import { Map, Obstacle } from '../../../models/map';
 
 @Component({
   selector: 'app-item-and-obstacle-test-page',
@@ -9,5 +10,29 @@ import { BattleGridComponent } from '../../shared/battle-grid/battle-grid.compon
   styleUrl: './item-and-obstacle-test-page.component.css'
 })
 export class ItemAndObstacleTestPageComponent {
+
+  createBattle() {
+    // create map dimensions, obstacles, items, and characters
+
+    const rock : Obstacle = {
+      position : {x: 2, y: 2},
+      image: "rock"
+    };
+
+    const tree : Obstacle = {
+      position :  {x: 4, y: 4},
+      image: "tree"
+    }
+
+    const map: Partial<Map> = {
+      width :10,
+      length : 5,
+      obstacles: [rock, tree]
+    }
+  }
+
+  loadBattle() {}
+
+  saveBattle() {}
 
 }
