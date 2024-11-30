2577e170383befe9c08330ac8d2fa5f0b59557b1
Nathan Arvan <nathannarvan@gmail.com>
2024-11-30 12:29:23 -0600

    update navigation

--- src/app/modules/test-pages/test-pages.module.ts
+++ src/app/modules/test-pages/test-pages.module.ts
@@ -2,9 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CampaignCreateTestPageComponent } from './campaign-create-test-page/campaign-create-test-page.component';
+import { LoadBattleTestPageComponent } from './load-battle-test-page/load-battle-test-page.component';
+import { ItemAndObstacleTestPageComponent } from './item-and-obstacle-test-page/item-and-obstacle-test-page.component';
 
 const routes : Routes = [
-
+  {
+    path: 'campaign-create',
+    component: CampaignCreateTestPageComponent
+  },
+  {
+    path: 'load-battle',
+    component: LoadBattleTestPageComponent
+  },
+  {
+    path: 'item-and-obstacle',
+    component: ItemAndObstacleTestPageComponent
+  }
 ]
 
 @NgModule({
